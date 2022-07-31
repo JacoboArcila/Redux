6e127f69e730eb29b8076ac3f3a9f20ffe27c102
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-07-31 17:40:42 -0500

    iniciando

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Searcher from './components/Searcher';
+import { Col } from 'antd';
+import PokemonList from './components/PokemonList'
 
 function App() {
   return (
     <div className="App">
-      Pokedux
+      <Col span={8} offset={8} >
+        <Searcher />
+      </Col>
+      <PokemonList />
     </div>
   );
 }
---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PokemonCard from './PokemonCard';
+import './PokemonList.css'
+
+const PokemonList = ({pokemons}) => {
+  return (
+    <div className='PokemonList'>
+        {
+            pokemons.map((pokemon, index) => (
+                <PokemonCard key={index} />
+            ))
+        }
+    </div>
+  )
+}
+
+PokemonList.defaultProps = {
+    pokemons: Array(10).fill('')
+}
+
+export default PokemonList
