f054e9b9f5af1fa3b7157d3f9ae63ff0789b7979
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-07-31 19:19:51 -0500

    middlewares (complejo)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { pokemonsReducer } from './reducers/pokemons';
-import { Provider } from 'react-redux/es/exports';
-import { legacy_createStore as createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { pokemonsReducer } from "./reducers/pokemons";
+import { Provider } from "react-redux/es/exports";
+import { applyMiddleware, compose, legacy_createStore as createStore } from "redux";
+import { featuring, logger, prefix } from "./middlewares";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const store = createStore(pokemonsReducer)
+//la constante composedEnhancers son potenciadores de la store
+//El primero es para habilitar la extencion de el navegador
+//El segundo es para añadirle el middleware, appyMiddleware es una funcion del redux que se encarga
+//de pasarle todos los parametros que tiene el logger, si no lo pones no pasan los parametros
+
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring, prefix)
+);
+const store = createStore(pokemonsReducer, composedEnhancers);
 root.render(
   <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
